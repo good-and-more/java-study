63a76bc5253429a76fe4a2094db54ad3f769e369
good-and-more <ilovesp20@outlook.com>
2020-10-15 00:17:37 +0800

    '完成Collection的常见方法学习'

--- 02_CollectionAndReflect/src/com/itheima/demo01/Collection/Demo01Collection.java
+++ 02_CollectionAndReflect/src/com/itheima/demo01/Collection/Demo01Collection.java
@@ -9,6 +9,11 @@ import java.util.Collection;
 *   共性的方法常用的如下：
 * public boolean add（E e）：把给定的对象添加到当前集合中
 * public boolean remove（E e）: 把给定的对象在当前集合中删除
+* public boolean contains(E e): 判断当前集合中是否包含给定的对象
+* public int size():返回集合中元素的个数
+* public Object[] toArray():把集合中的元素存储到数组中
+* public void clear()：清空集合中所有元素
+* public boolean isEmpty():判断当前集合是否为空
 * */
 public class Demo01Collection {
     public static void main(String[] args) {
@@ -28,14 +33,43 @@ public class Demo01Collection {
         /*
         * public boolean remove（E e）: 把给定的对象在当前集合中删除
         * */
-        coll.remove("赵六");
-        coll.remove("王五");
+        boolean b2 = coll.remove("赵六");
+        System.out.println("b2: "+ b2);
+        boolean b3 = coll.remove("王五");
+        System.out.println("b3: "+ b3);
 
         /*
         * public boolean contains(E e):判断当前集合中是否包含给定的对象
         * */
         System.out.println("集合中是否包含赵六"+coll.contains("赵六"));
         System.out.println("集合中是否包含张三"+coll.contains("张三"));
+
+        /*
+        * public int size():返回集合中元素的个数
+        * */
+        int size = coll.size();
+        System.out.println("集合中元素的个数为："+size);
+
+        /*
+        * public Object[] toArray():把集合中的元素存储到数组中,返回一个对象类型的数组，因为集合中什么类型都可以存，所以都转成对象
+        * */
+        Object[] arr = coll.toArray();
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+
+        /*
+        * public void clear()：清空集合中所有元素,但是不删除集合
+        * */
+        boolean b4 = coll.isEmpty();
+        System.out.println("b4:"+b4);
+        coll.clear();
+        System.out.println(coll);
+        /*
+        * public boolean isEmpty():判断当前集合是否为空
+        * */
+        boolean b5 = coll.isEmpty();
+        System.out.println("b5:"+ b5);
     }
 
 }
